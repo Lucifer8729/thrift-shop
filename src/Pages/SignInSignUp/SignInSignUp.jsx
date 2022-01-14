aa0b6ec417640d7a6a59a7be8bbf59877fcc6137
Anushka Choudhary <66861161+ac5865@users.noreply.github.com>
2022-01-14 14:31:56 +0530

    Update SignInSignUp.jsx
    Added signup module

--- src/Pages/SignInSignUp/SignInSignUp.jsx
+++ src/Pages/SignInSignUp/SignInSignUp.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React from 'react';
 
 const SignInSignUp = () => {
-  return (
-    <>
-      <h1>sign in page</h1>
-    </>
-  );
-};
+    return (
+        <>
+            <section className="signup">
+                <div className='container'>
+                    <div className='signup-content'>
+                        <div className='signup-form'>
+                            <h2 className='form-title'> Sign Up </h2> 
+                             <form className='register-form' id='register-form'>
+                                <div className='form-group'>
+                                    <label htmlFor='name'>                             
+                                    </label>
+                                    <input type="text" name="name" id="name" autoComplete='off' placeholder='Name'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='email'>                      
+                                    </label>
+                                    <input type="text" name="email" id="email" autoComplete='off' placeholder='Email'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='mobile'>                           
+                                    </label>
+                                    <input type="text" name="mobile" id="mobile" autoComplete='off' placeholder='Mobile'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='password'>                    
+                                    </label>
+                                    <input type="password" name="password" id="password" autoComplete='off' placeholder='Password'/>
+                                </div>
+                                <div className='form-group'>
+                                    <label htmlFor='confirm'>                          
+                                    </label>
+                                    <input type="password" name="confirm" id="confirm" autoComplete='off' placeholder='Confirm Password'/>
+                                </div>
+                                <div className='form-group form-button'>
+                                    <input type="submit" name="sign-up-submit" id="sign-up-submit" value="Sign Up" /> 
+                                </div>
+                                <div>
+                                    <h4>or</h4>
+                                </div>
+                                <div className='form-group form-button'>
+                                    <input type="submit" name="sign-up-submit" id="sign-up-submit" value="Sign Up using Google" /> 
+                                </div>
+                                <div>
+                                    <h5>Already have an account? Login</h5>
+                                </div>
+                            </form> 
 
-export default SignInSignUp;
+                            {/* <div className='signup-image'>
+                                <figure>
+                                    <img src={shopimg} alt="image" />
+                                </figure> 
+                            </div> */}
+                        </div>
+                    </div>
+                </div> 
+
+            </section>
+        </>
+    )
+}
+
+export default SignInSignUp
